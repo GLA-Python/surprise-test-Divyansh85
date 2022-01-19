4fa03902a50ce512ad25d6da12ae20d8838ef5c7
Divyansh85 <91238471+Divyansh85@users.noreply.github.com>
2022-01-19 12:03:09 +0530

    Q2) solution

---
+++ expanding_function.py
@@ -0,0 +1,11 @@
+a=eval(input())
+def expanding(a):
+    difference=[]
+    d=0
+    for i in range(len(a)-1):
+        d=a[i]-a[i+1]
+        difference.append(abs(d))
+    return(sorted(set(difference))==sorted(difference))
+    
+print(expanding(a))    
+    
